5458e8267d3c38604407b5205fffe216ae22496e
yurongkoh <yurongkoh96@gmail.com>
2025-03-07 15:49:52 +0000

    finished activity 6

--- src/student/flask_paralympics/routes.py
+++ src/student/flask_paralympics/routes.py
@@ -1,12 +1,12 @@
-from flask import Blueprint, render_template
+from flask import Blueprint, render_template, request
 
 # Define a Blueprint named 'main' for the main routes
 main = Blueprint('main', __name__)
 
 # Define a route for the main page
 @main.route('/')
+@main.route('/<name>')
 
-# Define a function called index that returns a simple message
-def index():
-    # return f"Hello!"
-    return render_template('index.html')
+# Define a function called index that returns a personalized message
+def index(name=None):
+    return render_template('index.html', name=name)
